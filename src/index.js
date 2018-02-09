ff0add0983e5284f3c7df18eceac96f53e33bc0d
Soldeplata Saketos Candela <soldeplatadeveloper@gmail.com>
2018-02-09 07:56:52 +0100

    moved store from index

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { applyMiddleware, createStore } from 'redux';
 import App from './components/App';
 import { Provider } from 'react-redux';
-import { promiseMiddleware } from './middleware';
-
-const defaultState = { appName: 'conduit', articles: null };
-const reducer = (state = defaultState, action) => {
-  switch (action.type) {
-    case 'TOGGLE':
-      return { ...state, checked: !state.checked };
-
-    case 'HOME_PAGE_LOADED':
-      return { ...state, articles: action.payload.articles };
-
-    default:
-      return state;
-  }
-}
-const store = createStore(reducer, applyMiddleware(promiseMiddleware));
-
+import store from './store';
 
 ReactDOM.render((
   <Provider store={store}>
