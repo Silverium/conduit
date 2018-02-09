7c53940d12acda3b4177a38098343c706b741da0
Soldeplata Saketos Candela <soldeplatadeveloper@gmail.com>
2018-02-09 07:38:50 +0100

    main view finished

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import App from './components/App';
 import { Provider } from 'react-redux';
+import { promiseMiddleware } from './middleware';
 
 const defaultState = { appName: 'conduit', articles: null };
 const reducer = (state = defaultState, action) => {
   switch (action.type) {
     case 'TOGGLE':
       return { ...state, checked: !state.checked };
+
+    case 'HOME_PAGE_LOADED':
+      return { ...state, articles: action.payload.articles };
+
     default:
       return state;
   }
 }
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(promiseMiddleware));
 
 
 ReactDOM.render((
